f0f60a98a4a0afa83dd1036e464f8bded225913f
Erjoh <erjik1902@gmail.com>
2023-11-03 19:52:04 +0600

    toolkit

---
+++ src/pages/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "../Layout/Layout";
+
+const Loading = () => {
+    return (
+        <Layout>
+            <div className="loader"></div>
+        </Layout>
+    );
+};
+
+export default Loading;
---
+++ src/redux/store/store.js
@@ -0,0 +1,7 @@
+
+
+export default configureStore({
+    reducer: {
+        cocktails: cocktailReducer,
+    }
+})
